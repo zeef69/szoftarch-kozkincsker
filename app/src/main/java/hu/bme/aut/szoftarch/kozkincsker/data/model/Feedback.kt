938d1ce94d3819e0a9ddc4a2baf365fa4cb586ce
zeef69 <78967605+zeef69@users.noreply.github.com>
2023-11-04 16:34:30 +0100

    Feedback osztály hozzáadva, MissionState és MissionVisibility törölve

---
+++ app/src/main/java/hu/bme/aut/szoftarch/kozkincsker/data/model/Feedback.kt
@@ -0,0 +1,19 @@
+package hu.bme.aut.szoftarch.kozkincsker.data.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import java.util.UUID
+
+@Parcelize
+data class Feedback(
+    var id: String = UUID.randomUUID().toString(),
+    var session: Session = Session(),
+    var user: User = User(),
+    var rate: Double = 0.0,
+    var comment: String = "",
+    var commentState: CommentVisibility = CommentVisibility.SHOW
+): Parcelable{
+    enum class CommentVisibility() {
+        SHOW, HIDE
+    }
+}
