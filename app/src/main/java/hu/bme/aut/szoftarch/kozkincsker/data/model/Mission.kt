75e80a4574d0203234ac3329954fb4cf892f8082
TheFlyingPiano99 <simonzolika1999@gmail.com>
2023-11-04 16:00:30 +0100

    Added properties to Mission

--- app/src/main/java/hu/bme/aut/szoftarch/kozkincsker/data/model/Mission.kt
+++ app/src/main/java/hu/bme/aut/szoftarch/kozkincsker/data/model/Mission.kt
@@ -3,8 +3,17 @@ package hu.bme.aut.szoftarch.kozkincsker.data.model
 import android.os.Parcelable
 import java.util.UUID
 import kotlinx.parcelize.Parcelize
+import java.time.Duration
+import java.util.Date
+
 @Parcelize
 data class Mission(
     var id: String = UUID.randomUUID().toString(),
-    var name: String = ""
+    var name: String = "",
+    var creationDate: Date,
+    var modificatiobDate: Date,
+    var timeToSolve: Duration,
+    var missionCode: String = "",
+    var missionTags : MutableList<MissionTag>,
+    var isPlayableWithoutModerator: Boolean = true
 ): Parcelable
