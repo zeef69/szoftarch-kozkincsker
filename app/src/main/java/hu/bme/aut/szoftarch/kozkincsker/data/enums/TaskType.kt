c15aac1e7c1ad4574bbf98b463ce48612bf74d1c
zeef69 <78967605+zeef69@users.noreply.github.com>
2023-11-25 00:59:04 +0100

    taskType 4/7

--- app/src/main/java/hu/bme/aut/szoftarch/kozkincsker/data/enums/TaskType.kt
+++ app/src/main/java/hu/bme/aut/szoftarch/kozkincsker/data/enums/TaskType.kt
@@ -6,19 +6,37 @@ import hu.bme.aut.szoftarch.kozkincsker.R
 enum class TaskType(val translation: Int, val checkable: Boolean) {
     @PropertyName("listed_answer")
     ListedAnswer(R.string.task_type_listed_answer, true){
-        override fun solutionCheck(designerAnswers: String, userAnswer: String) = true
+        override fun solutionCheck(designerAnswers: String, userAnswer: String) : Boolean{
+            val splitter =  '|'
+            var check = true
+            if(designerAnswers.split(splitter).size < 13) return false
+            var answerSize = designerAnswers.split(splitter)[0].toInt()
+            for (i in 0..<6){
+                if(i<answerSize){
+                    check = check && (designerAnswers.split(splitter)[2*i+1].toBoolean() ==
+                        userAnswer.split(splitter)[2*i+1].toBoolean())
+                }
+            }
+            return check
+        }
     },
     @PropertyName("number_answer")
     NumberAnswer(R.string.task_type_number_answer, true){
-        override fun solutionCheck(designerAnswers: String, userAnswer: String) = true
+        override fun solutionCheck(designerAnswers: String, userAnswer: String) : Boolean{
+            return designerAnswers == userAnswer
+        }
     },
     @PropertyName("date_answer")
     DateAnswer(R.string.task_type_date_answer, true){
-        override fun solutionCheck(designerAnswers: String, userAnswer: String) = true
+        override fun solutionCheck(designerAnswers: String, userAnswer: String) : Boolean{
+            return designerAnswers == userAnswer
+        }
     },
     @PropertyName("map_answer")
     MapAnswer(R.string.task_type_map_answer, true){
-        override fun solutionCheck(designerAnswers: String, userAnswer: String) = true
+        override fun solutionCheck(designerAnswers: String, userAnswer: String) : Boolean {
+            return true
+        }
     },
     @PropertyName("order_answer")
     OrderAnswer(R.string.task_type_order_answer, true){
