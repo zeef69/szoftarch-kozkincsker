ca976224323b6352d24679ae3a398501c4199e26
zeef69 <78967605+zeef69@users.noreply.github.com>
2023-11-25 16:43:23 +0100

    resource add

--- app/src/main/java/hu/bme/aut/szoftarch/kozkincsker/views/theme/Color.kt
+++ app/src/main/java/hu/bme/aut/szoftarch/kozkincsker/views/theme/Color.kt
@@ -4,3 +4,4 @@ import androidx.compose.ui.graphics.Color
 val Purple500 = Color(0xff2962ff)
 val Purple700 = Color(0xFF0039cb)
 val Teal200 = Color(0xffaed0ff)
+val Teal300 = Color(0xFF6C94CA)
--- app/src/main/java/hu/bme/aut/szoftarch/kozkincsker/views/theme/Theme.kt
+++ app/src/main/java/hu/bme/aut/szoftarch/kozkincsker/views/theme/Theme.kt
@@ -5,17 +5,22 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Teal200,
+    secondaryVariant = Teal300,
+    onSecondary = Color.White
 )
 
 private val DarkColorPalette = darkColors(
     primary = Purple500,
     primaryVariant = Purple700,
     secondary = Teal200,
+    secondaryVariant = Teal300,
+    onSecondary = Color.White
 
 )
 
